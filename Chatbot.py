e308904428cbe80a50997c8f71880cf71a0abc2f
Jonathan Amaya <amaya.jonathan@outlook.com>
2025-02-04 21:59:28 -0500

    Create Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,9 @@
+question = input("Hello! What is your name? ")
+name = f"Nice to meet you, {question}!"
+print (name)
+age = input("How old are you? ")
+bot_age = 3
+age_difference = int(age) - bot_age
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+color = input ("What's your favorite color? ")
+print(f"Oh, {color} is a beautiful color!")
